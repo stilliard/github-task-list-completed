bf6596fa0c5c75c518f274c43d39d5e6b0f4a2aa
Glitch (crystal-colony) <none>
2017-10-27 19:03:30 +0000

    Checkpoint

--- tests/index.js
+++ tests/index.js
@@ -1,1 +1,2 @@
-//tests go here. Tests use Mocha by default but you can easily replace this your own preference in package.json
+//Tests use Mocha by default but you can easily replace this your own preference in package.json
+//To read more about testing with Probot, visit https://probot.github.io/docs/testing/
