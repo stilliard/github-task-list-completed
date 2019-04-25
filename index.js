cd8634d11d255365900d73de8881d1fd9cec9671
Glitch (probot-hello-world) <none>
2019-04-25 19:02:58 +0000

    🎐🌒 Checkpoint ./package.json:2222562/5079 ./index.js:4109682/184

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 module.exports = (app) => {
-  // Your code here
+
   app.log('Yay! The app was loaded!')
 
-  // example of probot responding 'Hello World' to a new issue being opened
-  app.on('issues.opened', async context => {
+  app.on(['pull_request.opened', 'pull_request.edited'], async context => {
     // `context` extracts information from the event, which can be passed to
     // GitHub API calls. This will return:
     //   {owner: 'yourname', repo: 'yourrepo', number: 123, body: 'Hello World!}
