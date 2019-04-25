6b2abd0b426efddd026db98f81136cb6d657d498
Glitch (probot-hello-world) <none>
2019-04-25 19:24:58 +0000

    🖼🚛 Checkpoint ./index.js:4109682/2681

--- index.js
+++ index.js
@@ -4,28 +4,29 @@ module.exports = (app) => {
 
   // watch for pull requests & their changes
   app.on(['pull_request.opened', 'pull_request.edited', 'pull_request.synchronize'], async context => {
-    
+    const startTime = (new Date).toISOString();
+
     // lookup the pr body/description
     const pr = context.payload.pull_request;
     const body = pr.body;
     
     // check if it contains any not checked task list items
-    let check = {
+    const hasOutstandingTasks = body.includes("- [ ] ");
+
+    // send check back to GitHub
+    return context.github.checks.create(context.repo({
       name: 'task-list-completed',
       head_branch: '', // workaround for https://github.com/octokit/rest.js/issues/874
       head_sha: pr.head.sha,
-      status: 'in_progress',
-      started_at: new Date(newStatus.timeStart).toISOString(),
-    };
-    if (body.includes("- [ ] ")) {
-      // if it does, send a failed status
-      params = context.issue({body: 'Hello World!'});
-    } else {    
-      // otherwise, send a success status
-      params = context.issue({body: 'Hello World!'});
-    }
-
-    // Send status back to GitHub
-    return context.github.checks.create(context.repo(check));
+      status: 'completed',
+      conclusion: hasOutstandingTasks ? 'failure' : 'success',
+      started_at: startTime,
+      completed_at: (new Date).toISOString(),
+      output: {
+        title: hasOutstandingTasks ? 'Outstanding tasks' : 'Tasks completed',
+        summary: hasOutstandingTasks ? 'Tasks still remain to be completed' : 'All tasks have been completed',
+        text: 'We check if any task lists need completing before you can merge this PR'
+      }
+    }));
   });
 };
