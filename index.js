fb22b335a2af3cc8b6792651088917c2a8954131
Gail Terman <gail_terman@symantec.com>
2020-07-31 14:17:50 -0400

    add done is done

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
+/* jslint esversion:8*/
 const marked = require('marked');
+
+const tasks = [
+  "step 1",
+  "step 2",
+  "step 3"
+];
+
+const build_template = () => {
+  let template = "\n<!-- start of DoD -->\n## Done is Done\n";
+  for (let i in tasks) {
+    template += `- [ ] ${tasks[i]}\n`;
+  }
+  template += "<!-- end of DoD -->";
+  return template;
+};
 
 module.exports = (app) => {
   app.log('Yay! The app was loaded!');
 
+  app.on(['pull_request.opened', 'pull_request.edited'], async context => {
+    // append checks to body if they don't exist
+    let body = context.payload.pull_request.body;
+
+    if (!body.includes("<!-- end of DoD -->")) {
+      body += build_template();
+      // fix deprecation of number
+      pull = context.issue({body});
+      pull.pull_number = pull.number;
+      delete pull.number;
+      return context.github.pulls.update(pull);
+    }
+  });
+
   // watch for pull requests & their changes
   app.on(['pull_request.opened', 'pull_request.edited', 'pull_request.synchronize'], async context => {
-    const startTime = (new Date).toISOString();
+    const startTime = (new Date()).toISOString();
 
     // lookup the pr body/description
     const pr = context.payload.pull_request;
