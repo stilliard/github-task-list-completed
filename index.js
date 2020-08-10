36bf7a438012e5e84ca6601b429823a82019a758
Andrew Stilliard <andrew.stilliard@gmail.com>
2020-08-10 23:07:00 +0100

    feat(check): now checking for task lists in PR comments

--- index.js
+++ index.js
@@ -4,18 +4,65 @@ module.exports = (app) => {
   app.log('Yay! The app was loaded!');
 
   // watch for pull requests & their changes
-  app.on(['pull_request.opened', 'pull_request.edited', 'pull_request.synchronize'], async context => {
+  app.on([
+    'pull_request.opened',
+    'pull_request.edited',
+    'pull_request.synchronize',
+    'issue_comment', // for comments on github issues
+    'pull_request_review_comment', // comment lines on diffs for reviews
+  ], async context => {
     const startTime = (new Date).toISOString();
 
-    // lookup the pr body/description
-    const pr = context.payload.pull_request;
-    const body = pr.body;
+    // lookup the pr
+    let pr = context.payload.pull_request;
 
-    const tokens = marked.lexer(body, { gfm: true });
-    const listItems = tokens.filter(token => token.type === 'list_item_start');
+    // check if this is an issue rather than pull event
+    if (context.event == 'issue_comment' && ! pr) {
+      app.log.debug('checking issue comment...');
+      // if so we need to make sure this is for a PR only
+      if (! context.payload.issue.pull_request) {
+        app.log.debug('issue is not PR');
+        return;
+      }
+      // & lookup the PR it's for to continue
+      app.log.debug('PR number is:', context.payload.issue.number);
+      let response = await context.github.pulls.get(context.repo({
+        pull_number: context.payload.issue.number
+      }));
+      pr = response.data;
+    }
+
+    let tokens = marked.lexer(pr.body, { gfm: true });
+    let listItems = tokens.filter(token => token.type === 'list_item_start');
     
     // check if it contains any not checked task list items
-    const hasOutstandingTasks = listItems.some(item => item.checked === false);
+    let hasOutstandingTasks = listItems.some(item => item.checked === false);
+
+    // lookup comments on the PR
+    let comments = await context.github.issues.listComments(context.repo({
+      issue_number: pr.number
+    }));
+    app.log.debug('found comments:', comments.data.length, comments.data);
+
+    // as well as review comments
+    let reviewComments = await context.github.pulls.listComments(context.repo({
+      pull_number: pr.number
+    }));
+    app.log.debug('found review-comments:', reviewComments.data.length);
+    if (reviewComments.data.length) {
+      comments.data = comments.data.concat(reviewComments.data);
+    }
+
+    // & check them for tasks
+    if (comments.data.length) {
+      comments.data.forEach(function (comment) {
+        let tokens = marked.lexer(comment.body, { gfm: true });
+        let listItems = tokens.filter(token => token.type === 'list_item_start');
+        if (listItems.some(item => item.checked === false)) {
+          hasOutstandingTasks = true;
+        }
+      });
+    }
 
     let check = {
       name: 'task-list-completed',
