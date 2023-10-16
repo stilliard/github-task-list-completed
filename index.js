c348b997f75fa3b94ca14df04abd186de545031b
Andrew Stilliard <andrew.stilliard@gmail.com>
2023-10-16 20:20:27 +0100

    fix(errors): Better error detection added around each github api request

--- index.js
+++ index.js
@@ -5,13 +5,6 @@ const ENABLE_ID_LOGS = true; // Repo name & ID only for logs, no private data lo
 module.exports = (app) => {
   app.log('Yay! The app was loaded!');
 
-  // log helper
-  function log(pr, message) {
-    if (ENABLE_ID_LOGS) {
-      app.log(`PR ${pr.head.repo.full_name}#${pr.number}: ${message}`);
-    }
-  }
-
   // watch for pull requests & their changes
   app.on([
     'pull_request.opened',
@@ -21,6 +14,13 @@ module.exports = (app) => {
     'pull_request_review', // reviews
     'pull_request_review_comment', // comment lines on diffs for reviews
   ], async context => {
+
+    // log helper
+    function log(pr, message, type = 'info') {
+      if (ENABLE_ID_LOGS) {
+        context.log[type](`PR ${pr.head.repo.full_name}#${pr.number}: ${message}`);
+      }
+    }
 
     // lookup the pr
     let pr = context.payload.pull_request;
@@ -32,13 +32,17 @@ module.exports = (app) => {
         return;
       }
       // & lookup the PR it's for to continue
-      let response = await context.octokit.pulls.get(context.repo({
-        pull_number: context.payload.issue.number
-      }));
-      pr = response.data;
+      try {
+        let response = await context.octokit.pulls.get(context.repo({
+          pull_number: context.payload.issue.number
+        }));
+        pr = response.data;
+      } catch (err) {
+        context.log.error(`Error looking up PR, skipping. Error (${err.status}): ${err.message}`);
+      }
     }
     if (! pr) {
-      app.log('[error] not on a PR?');
+      context.log.error('Not on a PR?');
       return;
     }
 
@@ -56,37 +60,51 @@ module.exports = (app) => {
 
 
     // lookup comments on the PR
-    let comments = await context.octokit.issues.listComments(context.repo({
-      per_page: 100,
-      issue_number: pr.number
-    }));
+    let comments;
+    try {
+      comments = await context.octokit.issues.listComments(context.repo({
+        per_page: 100,
+        issue_number: pr.number
+      }));
+    } catch (err) {
+      log(pr, `Error looking up comments, skipping. Error (${err.status}): ${err.message}`, 'error');
+    }
 
     log(pr, 'Main comments api lookup complete');
 
     // as well as review comments
-    let reviewComments = await context.octokit.pulls.listReviews(context.repo({
-      per_page: 100,
-      pull_number: pr.number
-    }));
-    if (reviewComments.data.length) {
-      comments.data = comments.data.concat(reviewComments.data);
+    let reviewComments;
+    try {
+      reviewComments = await context.octokit.pulls.listReviews(context.repo({
+        per_page: 100,
+        pull_number: pr.number
+      }));
+      if (reviewComments.data.length) {
+        comments.data = comments.data.concat(reviewComments.data);
+      }
+    } catch (err) {
+      log(pr, `Error looking up review comments, skipping. Error (${err.status}): ${err.message}`, 'error');
     }
 
     log(pr, 'Review comments api lookup complete');
 
     // and diff level comments on reviews
-    let reviewDiffComments = await context.octokit.pulls.listReviewComments(context.repo({
-      per_page: 100,
-      pull_number: pr.number
-    }));
-    if (reviewDiffComments.data.length) {
-      comments.data = comments.data.concat(reviewDiffComments.data);
+    try {
+      let reviewDiffComments = await context.octokit.pulls.listReviewComments(context.repo({
+        per_page: 100,
+        pull_number: pr.number
+      }));
+      if (reviewDiffComments.data.length) {
+        comments.data = comments.data.concat(reviewDiffComments.data);
+      }
+    } catch (err) {
+      log(pr, `Error looking up review diff comments, skipping. Error (${err.status}): ${err.message}`, 'error');
     }
 
     log(pr, 'Diff comments api lookup complete');
 
     // & check them for tasks
-    if (comments.data.length) {
+    if (comments && comments.data && comments.data.length) {
       comments.data.forEach(function (comment) {
         let commentOutstandingTasks = checkOutstandingTasks(comment.body);
         outstandingTasks.total += commentOutstandingTasks.total;
@@ -128,8 +146,12 @@ module.exports = (app) => {
     log(pr, 'Complete and sending back to GitHub');
 
     // send check back to GitHub
-    const response = await context.octokit.checks.create(context.repo(check));
-    log(pr, `Check response status from GitHub ${response.status} [X-GitHub-Request-Id: ${response.headers['x-github-request-id']}]`);
+    try {
+      const response = await context.octokit.checks.create(context.repo(check));
+      log(pr, `Check response status from GitHub ${response.status} [X-GitHub-Request-Id: ${response.headers['x-github-request-id']}]`);
+    } catch (err) {
+      log(pr, `Error sending check back to GitHub. Error (${err.status}): ${err.message}`, 'error');
+    }
 
     return;
   });
